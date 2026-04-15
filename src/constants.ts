788fa9dbc1922e442f161b6ec207740c5c267832
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2026-04-15 09:58:22 +0200

    feat: add EXPORT_CHIPS constant for export-type chip groupings

---
+++ src/constants.ts
@@ -0,0 +1,43 @@
+// src/constants.ts
+
+export const EXPORT_CHIPS: {
+  key: string
+  label: string
+  sub: string
+  groups: string[]
+}[] = [
+  {
+    key: 'tariff',
+    label: 'Tariff',
+    sub: 'struktur',
+    groups: [
+      'Tariff', 'FareZone', 'FareStructureElement', 'GeographicStructureFactor',
+      'QualityStructureFactor', 'TimeStructureFactor', 'DistanceMatrixElement',
+      'FareSeries', 'ValidableElement',
+    ],
+  },
+  {
+    key: 'fareproduct',
+    label: 'FareProduct',
+    sub: 'typer',
+    groups: ['FareProduct'],
+  },
+  {
+    key: 'faretable',
+    label: 'FareTable',
+    sub: 'priser',
+    groups: ['FareTable', 'FarePrice', 'PricingRule'],
+  },
+  {
+    key: 'salesoffer',
+    label: 'SalesOffer',
+    sub: 'salg',
+    groups: ['SalesOfferPackage', 'DistributionChannel', 'FulfilmentMethod', 'TypeOfTravelDocument'],
+  },
+  {
+    key: 'usageparam',
+    label: 'UsageParam',
+    sub: 'vilkår',
+    groups: ['UsageParameter'],
+  },
+]
